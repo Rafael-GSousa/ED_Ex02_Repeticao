d185c258a2b4aa3d357617a0faa4545b0104e95b
rafae <rafae@DESKTOP-GBGPNIQ>
2021-08-23 02:15:45 -0300

    ED_Ex02_Repeticao

--- src/controller/RepeticaoController.java
+++ src/controller/RepeticaoController.java
@@ -8,16 +8,23 @@ public class RepeticaoController {
 	
 	public int repeticao(int val1, int val2) {
 		
+/*		condição de parada, se a primeira entrada for zero,
+   		então o retorno é zero */
+ 
 		if(val1 == 0) {
 			return 0;
-		}else {
+		}
+/*		senão, a primeira entrada será dividida por 10
+		e o contador iniciado em zero */ 		
+		else {
 		int div = val1 / 10;
 		int cont = 0;
-		
+/*		se o resto da divisão da primeira entrada for igual
+		a segunda entrada, o contador recebe +1*/
 		if(val1 % 10 == val2) {
 			cont ++;
 		}
-		
+//		o retorno é o valor acumulado do contador 
 		return cont + repeticao(div, val2);
 		}
 	}
--- src/view/Main.java
+++ src/view/Main.java
@@ -7,19 +7,25 @@ import controller.RepeticaoController;
 public class Main {
 
 	public static void main(String[] args) {
+		
 		RepeticaoController rep = new RepeticaoController();
-		
+
+//		definindo a 1ª entrada na caixa de diálogo
 		String n1 = JOptionPane.showInputDialog("Digite o primeiro valor inteiro de 10 a 999999");
+//		convertendo para inteiro
 		int num1 = Integer.parseInt(n1);
-		
+/*		se a 1ª entrada não atender ao que foi estipulado
+		será exibida uma mensagem e a aplicação será encerrada */
 		if(num1 < 10 || num1 > 999999) {
 			System.out.println("Entrada inválida");
 			System.exit(0);
 		}
-		
+//		definindo a 2ª entrada na caixa de diálogo		
 		String n2 = JOptionPane.showInputDialog("Digite o segundo valor inteiro de 0 a 9");
+//		convertendo para inteiro
 		int num2 = Integer.parseInt(n2);
-		
+/*		se a 2ª entrada não atender ao que foi estipulado
+		será exibida uma mensagem e a aplicação será encerrada */		
 		if(num2 < 0 || num2 > 9) {
 			System.out.println("Entrada inválida");
 			System.exit(0);
